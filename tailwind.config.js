02875d660ccb3b495d9e7a301380f64323c9cfec
Neema Sadry <snake117@gmail.com>
2023-02-03 15:37:32 -0500

    Created and migrated database. Added NPM packages via Yarn. Configured tailwind.config.js.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,37 @@
+const colors = require('tailwindcss/colors')
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
+  plugins: [
+    require('@tailwindcss/aspect-ratio'),
+    require('@tailwindcss/forms'),
+    require('@tailwindcss/line-clamp'),
+    require('@tailwindcss/typography'),
+  ],
   content: [
-    './app/views/**/*.html.erb',
+    './app/views/**/*.erb',
+    './app/views/**/*.haml',
+    './app/views/**/*.slim',
     './app/helpers/**/*.rb',
     './app/assets/stylesheets/**/*.css',
     './app/javascript/**/*.js'
-  ]
+  ],
+  // All the default values will be compiled unless they are overridden below
+  theme: {
+    // Extend (add to) the default theme in the `extend` key
+    extend: {
+      // Create your own at: https://javisperez.github.io/tailwindcolorshades
+      colors: {
+        primary: colors.slate,
+        secondary: colors.emerald,
+        tertiary: colors.gray,
+        danger: colors.red,
+        "code-400": "#fefcf9",
+        "code-600": "#3c455b",
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
 }
